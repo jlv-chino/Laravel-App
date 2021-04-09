2c11b5c784cf4d40f2b5d305ac22e28d80a54f19
José Luis Vilches <jose.luis.comvilches@gmail.com>
2021-04-09 02:09:54 -0300

    Agregamos las rutas para el administrador

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AdminUsersController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +22,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('/admin/users', 'AdminUsersController');
